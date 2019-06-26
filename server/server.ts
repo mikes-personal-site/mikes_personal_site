670577593b54b04e650ce77f04c9dfad36c15cbc
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-06-25 17:31:28 -0700

    Home route now serves HTML

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,16 @@
 import * as express from "express";
+import * as serve from "express-static";
+import * as path from "path";
+
 const app: express.Application = express();
 const PORT: number = 3000;
+
+app.use("/client", serve(path.resolve(__dirname, "client")));
 
 app.get(
   "/",
   (req: express.Request, res: express.Response): void => {
-    res.send("You made it to Mikes Personal Page!");
+    res.sendfile(path.resolve(__dirname, "../client/index.html"));
   }
 );
 
