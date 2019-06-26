ed8dba0768b49be7c55bedd1dcb85f8917ffa515
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-06-25 19:01:29 -0700

    Added code to append h tag

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,4 +1,4 @@
-function main() {
+function main(){
   const h2 = document.createElement("h2");
   h2.setAttribute("id", "h3Root");
   h2.innerHTML = "This text was created dynamically with JavaScript.";
