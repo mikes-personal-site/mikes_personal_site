5cbb750f5951459ca0282b8935d75ed58d6a5976
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-06-25 18:49:22 -0700

    Added h tags to HTML

---
+++ client/assets/index.js
@@ -0,0 +1,6 @@
+function main() {
+  const h2 = document.createElement("h2");
+  h2.setAttribute("id", "h3Root");
+  h2.innerHTML = "This text was created dynamically with JavaScript.";
+  document.getElementById("Root").appendChild(h2);
+};
--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,15 @@
 import * as express from "express";
-import * as serve from "express-static";
 import * as path from "path";
 
 const app: express.Application = express();
 const PORT: number = 3000;
 
-app.use("/client", serve(path.resolve(__dirname, "client")));
+app.use("/", express.static(path.resolve(__dirname, "../client/assets")));
 
 app.get(
   "/",
   (req: express.Request, res: express.Response): void => {
-    res.sendfile(path.resolve(__dirname, "../client/index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/index.html"));
   }
 );
 
