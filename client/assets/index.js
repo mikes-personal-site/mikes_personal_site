ba91796850407f0ba723728229f562a2095072a2
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-06-26 18:15:53 -0700

    Changed file structure

--- client/assets/index.js
+++
@@ -1,6 +0,0 @@
-function main(){
-  const h2 = document.createElement("h2");
-  h2.setAttribute("id", "h3Root");
-  h2.innerHTML = "This text was created dynamically with JavaScript.";
-  document.getElementById("Root").appendChild(h2);
-}
