76729b24b0a1ad9cd3b6bfd633f73598cef8cfda
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-07-01 16:51:22 -0700

    Added event listener for new project button

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -3,4 +3,19 @@ function main(){
   h2.setAttribute("id", "h3Root");
   h2.innerHTML = "This text was created dynamically with JavaScript.";
   document.getElementById("Root").appendChild(h2);
+  second()
+}
+
+function second() {
+  const button = document.getElementById("projectButton");
+  button.addEventListener("click", getProjects);
+}
+
+// Still need to get fetch to return proper JSON!
+function getProjects() {
+  fetch('http://localhost:3000/projectButton')
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+    })
 }
