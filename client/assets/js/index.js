b929e89e2f2f337dd67cdc81f50e3959c27713ae
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-07-02 20:15:49 -0700

    Trying to debug the fetch for projects

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -3,19 +3,24 @@ function main(){
   h2.setAttribute("id", "h3Root");
   h2.innerHTML = "This text was created dynamically with JavaScript.";
   document.getElementById("Root").appendChild(h2);
-  second()
-}
-
-function second() {
   const button = document.getElementById("projectButton");
   button.addEventListener("click", getProjects);
 }
 
-// Still need to get fetch to return proper JSON!
+// I Think the line 18 is the problem, .json() is not working.
 function getProjects() {
-  fetch('http://localhost:3000/projectButton')
-    .then((response) => response.json())
+  fetch("http://localhost:3000/projectButton", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then((response) => response.toString())
     .then((response) => {
       console.log(response);
     })
+    .catch(err => {
+      // Do something for an error here
+      console.log("Error Reading data " + err);
+    });
 }
