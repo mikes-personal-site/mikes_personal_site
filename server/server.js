d2576deee9c2babf35895b7ba7025d9b6bdbc4fe
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-07-31 18:46:59 -0700

    Removed unused files

--- server/server.js
+++ server/server.js
@@ -4,21 +4,9 @@ var express = require("express");
 var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
-app.use("/", express.static(path.resolve(__dirname, "../client")));
+app.use("/", express.static(path.resolve(__dirname, "../client/assets")));
 app.get("/", function (req, res) {
     res.sendFile(path.resolve(__dirname, "../client/views/index.html"));
-});
-app.get("/projects", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/views/projects.html"));
-});
-app.get("/stylesProjects.css", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/assets/styles/stylesProjects.css"));
-});
-app.get("/contact", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/views/contact.html"));
-});
-app.get("/stylesContact.css", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/assets/styles/stylesContact.css"));
 });
 app.listen(PORT, function () {
     var newLocal = "listing on http://localhost:" + PORT + "/";
--- server/server.ts
+++ server/server.ts
@@ -2,42 +2,12 @@ import * as express from "express";
 import * as path from "path";
 const app: express.Application = express();
 const PORT = process.env.PORT || 8080;
-app.use("/", express.static(path.resolve(__dirname, "../client")));
+app.use("/", express.static(path.resolve(__dirname, "../client/assets")));
 
 app.get(
   "/",
   (req: express.Request, res: express.Response): void => {
     res.sendFile(path.resolve(__dirname, "../client/views/index.html"));
-  }
-);
-
-app.get(
-  "/projects",
-  (req: express.Request, res: express.Response): void => {
-    res.sendFile(path.resolve(__dirname, "../client/views/projects.html"));
-  }
-);
-app.get(
-  "/stylesProjects.css",
-  (req: express.Request, res: express.Response): void => {
-    res.sendFile(
-      path.resolve(__dirname, "../client/assets/styles/stylesProjects.css")
-    );
-  }
-);
-
-app.get(
-  "/contact",
-  (req: express.Request, res: express.Response): void => {
-    res.sendFile(path.resolve(__dirname, "../client/views/contact.html"));
-  }
-);
-app.get(
-  "/stylesContact.css",
-  (req: express.Request, res: express.Response): void => {
-    res.sendFile(
-      path.resolve(__dirname, "../client/assets/styles/stylesContact.css")
-    );
   }
 );
 
