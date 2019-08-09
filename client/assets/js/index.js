3b13d17e3098c6c87ff1553aa65fe3bae82153fa
Mike Mulderink <mulderink1@gmail.com>
2019-08-08 23:07:09 -0700

    Added clipboard func

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,8 +1,9 @@
-
 const modal = document.getElementById("myModal"),
   btn = document.getElementById("myBtn"),
   span = document.getElementsByClassName("close")[0],
-  btn2nd = document.getElementById("myBtn2nd");
+  btn2nd = document.getElementById("myBtn2nd"),
+  email = document.querySelector(".email").innerHTML,
+  el = document.createElement("textarea");
 
 btn.onclick = function () {
   modal.style.display = "block";
@@ -20,4 +21,13 @@ window.onclick = function (event) {
 
 btn2nd.onclick = function () {
   modal.style.display = "block";
+};
+
+function myFunction() {
+  document.body.appendChild(el);
+  el.value = email;
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  alert("Copied to Clipboard");
 };
