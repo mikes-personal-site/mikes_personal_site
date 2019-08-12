523e9a032c4e46ceb3c7d8cec027db5709745fbb
Mike Mulderink <mulderink1@gmail.com>
2019-08-12 13:05:14 -0700

    Removed clipboard function from mobile view

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,33 +1,35 @@
-const modal = document.getElementById("myModal"),
-  btn = document.getElementById("myBtn"),
-  span = document.getElementsByClassName("close")[0],
-  btn2nd = document.getElementById("myBtn2nd"),
-  email = document.querySelector(".email").innerHTML,
-  el = document.createElement("textarea");
+if (window.innerWidth > 600) {
+  const modal = document.getElementById("myModal"),
+    btn = document.getElementById("myBtn"),
+    span = document.getElementsByClassName("close")[0],
+    btn2nd = document.getElementById("myBtn2nd"),
+    email = document.querySelector(".email").innerHTML,
+    el = document.createElement("textarea");
 
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  btn.onclick = function () {
+    modal.style.display = "block";
   }
-};
 
-btn2nd.onclick = function () {
-  modal.style.display = "block";
-};
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 
-function myFunction() {
-  document.body.appendChild(el);
-  el.value = email;
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  alert("Copied to Clipboard");
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  btn2nd.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  function myFunction() {
+    document.body.appendChild(el);
+    el.value = email;
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert("Copied to Clipboard");
+  };
 };
